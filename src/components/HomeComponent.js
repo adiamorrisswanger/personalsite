5a20dec9898fd3f2b3e9bfb11a1797b75ae8ca22
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2022-06-01 21:08:58 -0500

    Added Bootstrap and Reactstrap

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import { Button } from 'reactstrap';
 
 
 function Home() {
 
-    const [title, setTitle] = useState(0)
-    const values = ['Dancer','Dog Trainer', 'Non Profit Admin', 'Actor', 'Choreographer', 'TV Host'];
+    const [title, setTitle] = useState(null)
+    const values = ['Dancer','Dog Trainer', 'Non Profit Admin', 'Actor', 'Choreographer', 'TV Host', 'Cater Waiter', 'Tour Guide'];
 
     return (
-        <div>
+        <div className='home'>
             <div>
-                <p>Adia Morris Swanger | Web Developer | {title}</p>
-                <button onClick={() => setTitle(values[Math.floor(Math.random() * values.length)])}></button>
+                <h1>Adia Morris Swanger | Web Developer and...{title}</h1>
+                <Button color='info' onClick={() => setTitle(values[Math.floor(Math.random() * values.length)])}>Change It Up</Button>
             </div>
             
         </div>
