ec282c608e8d5d3bf70bd20fdecd1a5a759fab66
Adia Morris Swanger <adiamorrisswanger@gmail.com>
2022-06-15 22:19:09 -0500

    Tweaking footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -12,18 +12,21 @@ function Footer() {
         <div className='footer'>
              <div className='container'>
                     <div className='row'>
-                        <div className='col-sm-2'>
+                        <div className='col-md-2'>
                             <div>
-                                <a className='btn btn-social-icon' href='https://www.linkedin.com/in/adia-morris-swanger/'><i className="fa fa-brands fa-linkedin" ></i> LinkedIn</a> 
+                                <a className='btn btn-social-icon' href='https://www.linkedin.com/in/adia-morris-swanger/' target='_blank' rel='noopener noreferrer'><i className="fa fa-brands fa-linkedin" ></i> LinkedIn</a> 
                             </div>
                             <div>
-                                <a className='btn btn-social-icon' href='https://github.com/adiamorrisswanger'><i className="fa fa-brands fa-github" ></i> GitHub</a> 
+                                <a className='btn btn-social-icon' href='https://github.com/adiamorrisswanger' target='_blank' rel='noopener noreferrer'><i className="fa fa-brands fa-github" ></i> GitHub</a> 
                             </div>
                             <div>
-                                <a className='btn btn-social-icon' href='https://twitter.com/longtallsallie'><i className="fa fa-brands fa-twitter-square social-icon" ></i> Twitter</a> 
+                                <a className='btn btn-social-icon' href='https://twitter.com/longtallsallie' target='_blank' rel='noopener noreferrer'><i className="fa fa-brands fa-twitter-square social-icon" ></i> Twitter</a> 
                             </div>
                         </div>
-                        <div className='col-sm-10'>
+                        <div className='col-md-4'>
+
+                        </div>
+                        <div className='col-md-6 mt-5'>
                             I've also been...<Button onClick={() => setTitle(values[Math.floor(Math.random() * values.length)])}>{title}.</Button> 
                         </div>
                         
